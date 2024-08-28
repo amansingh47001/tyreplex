234431c6fcd50bc283d1783b692651e70b653b2c
Aman Singh <amanyup8459@gmail.com>
2024-08-28 23:20:35 +0530

    changed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
+    // output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    basePath: "/tyreplex",
+    basePath: '/tyreplex',
     images: {
         domains: ["cdn.tyreplex.net"]
     }
